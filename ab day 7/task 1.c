33b6e4f666d3d36354e6e8b453ee00f46bda0ffa
absabhadiya <134709692+absabhadiya@users.noreply.github.com>
2023-05-31 18:38:07 +0530

    Add files via upload

---
+++ ab day 7/task 1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main (){
+	
+	int a,b;
+	 
+	 printf("enter value a: ");
+	 scanf("%d",&a);
+	 
+	 printf("enter value b: ");
+	 scanf("%d",&b);
+	 
+	 if (a>b){
+	 	
+	 	printf("a is greterthen b");
+	 }	
+	else{
+		
+		printf("b is greterthen a");
+		
+	}
+	return 0;
+}
---
+++ ab day 7/task 2.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main (){
+	
+	int a;
+	 
+	 printf("enter value a: ");
+	 scanf("%d",&a);
+	 
+	 
+	 if (a>0){
+	 	
+	 	printf("a is positiev");
+	 }	
+	else if (a<0){
+		
+		printf("a is negative");
+		
+	}
+	else {  
+	      printf("nuteral");
+	
+	}
+	return 0;
+}
